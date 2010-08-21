2057a02786f774a0c015bd3771f2029b0eff2c93
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-21 20:09:45 +0000

    Initial import from Kickstarter code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_sql/trunk@37097 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "svconnector_sql".
+#
+# Auto generated 21-08-2010 22:05
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SQL-based Database Connector',
+	'description' => 'Connector service for any SQL-based database via ADODB.',
+	'category' => 'services',
+	'author' => 'Francois Suter (Cobweb)',
+	'author_email' => 'typo3@cobweb.ch',
+	'shy' => '',
+	'dependencies' => 'adodb',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'adodb' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"6ec1";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4c93";s:19:"doc/wizard_form.dat";s:4:"3356";s:20:"doc/wizard_form.html";s:4:"4056";s:35:"sv1/class.tx_svconnectorsql_sv1.php";s:4:"bf92";}',
+);
+
+?>
