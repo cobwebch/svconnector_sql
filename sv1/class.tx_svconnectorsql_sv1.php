8734d177619de1b0b7ac82ed09ed0049d7c82efc
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-15 12:07:52 +0000

    Code clean up
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_sql/trunk@63483 735d13b6-9817-0410-8766-e36946ffe9aa

--- sv1/class.tx_svconnectorsql_sv1.php
+++ sv1/class.tx_svconnectorsql_sv1.php
@@ -28,6 +28,8 @@ require_once(t3lib_extMgm::extPath('adodb', 'adodb/adodb-exceptions.inc.php'));
 /**
  * Service "SQL connector" for the "svconnector_sql" extension.
  *
+ * Uses ADODB to connect to a variety of DBMS
+ *
  * @author		Francois Suter (Cobweb) <typo3@cobweb.ch>
  * @package		TYPO3
  * @subpackage	tx_svconnectorsql
@@ -42,7 +44,6 @@ class tx_svconnectorsql_sv1 extends tx_svconnector_base {
 	/**
 	 * Verifies that the connection is functional
 	 * In this case it always is, as the connection can really be tested only for specific configurations
-	 *
 	 * @return	boolean		TRUE if the service is available
 	 */
 	public function init() {
@@ -56,7 +57,6 @@ class tx_svconnectorsql_sv1 extends tx_svconnector_base {
 	 * i.e. the SQL record set, but without any additional work performed on it
 	 *
 	 * @param	array	$parameters: parameters for the call
-	 *
 	 * @return	mixed	server response
 	 */
 	public function fetchRaw($parameters) {
@@ -77,7 +77,6 @@ class tx_svconnectorsql_sv1 extends tx_svconnector_base {
 	 * This method calls the query and returns the results from the response as an XML structure
 	 *
 	 * @param	array	$parameters: parameters for the call
-	 *
 	 * @return	string	XML structure
 	 */
 	public function fetchXML($parameters) {
@@ -99,9 +98,9 @@ class tx_svconnectorsql_sv1 extends tx_svconnector_base {
 	/**
 	 * This method calls the query and returns the results from the response as a PHP array
 	 *
-	 * @param	array	$parameters: parameters for the call
-	 *
-	 * @return	array	PHP array
+	 * @param array $parameters Parameters for the call
+	 * @throws Exception
+	 * @return array PHP array
 	 */
 	public function fetchArray($parameters) {
 		try {
