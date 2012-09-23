9b67e5289de5e7f86b582393279b16cc67ba9b83
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-23 16:45:07 +0000

    Released version 1.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_sql/trunk@66290 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "svconnector_sql".
 #
-# Auto generated 15-06-2012 15:15
+# Auto generated 23-09-2012 18:44
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.7.99',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"f74b";s:10:"README.txt";s:4:"945c";s:16:"ext_autoload.php";s:4:"a3ed";s:21:"ext_conf_template.txt";s:4:"d058";s:12:"ext_icon.gif";s:4:"73a1";s:17:"ext_localconf.php";s:4:"4c93";s:13:"locallang.xml";s:4:"13e9";s:14:"doc/manual.pdf";s:4:"1ee0";s:14:"doc/manual.sxw";s:4:"c653";s:35:"sv1/class.tx_svconnectorsql_sv1.php";s:4:"e409";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"0908";s:16:"ext_autoload.php";s:4:"a3ed";s:21:"ext_conf_template.txt";s:4:"d058";s:12:"ext_icon.gif";s:4:"73a1";s:17:"ext_localconf.php";s:4:"4c93";s:13:"locallang.xml";s:4:"13e9";s:10:"README.txt";s:4:"945c";s:42:"Resources/Public/Samples/Configuration.txt";s:4:"74ed";s:14:"doc/manual.pdf";s:4:"07f8";s:14:"doc/manual.sxw";s:4:"f7e5";s:35:"sv1/class.tx_svconnectorsql_sv1.php";s:4:"bd74";}',
 	'suggests' => array(
 	),
 );
