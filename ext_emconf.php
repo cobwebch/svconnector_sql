05388cdcbb8dbc26233d8fd1b60b305adbfc8a56
Francois Suter <francois@typo3.org>
2014-04-02 17:34:21 +0200

    [TASK] Verify compatibility with TYPO3 6.2
    Nothing to do :-)
    
    Resolves: #57569
    Releases: 1.4
    Change-Id: I560884070a5874661110bb48b5471a303ba16f49

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,23 +28,23 @@ $EM_CONF[$_EXTKEY] = array (
   'lockType' => '',
   'author_company' => '',
   'version' => '1.3.0',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
-      'typo3' => '4.5.0-6.1.99',
+      'typo3' => '4.5.0-6.2.99',
       'adodb' => '',
       'svconnector' => '2.2.0-0.0.0',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
   '_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"4924";s:16:"ext_autoload.php";s:4:"a3ed";s:21:"ext_conf_template.txt";s:4:"d058";s:12:"ext_icon.gif";s:4:"73a1";s:17:"ext_localconf.php";s:4:"4c93";s:13:"locallang.xml";s:4:"13e9";s:10:"README.txt";s:4:"945c";s:42:"Resources/Public/Samples/Configuration.txt";s:4:"74ed";s:14:"doc/manual.pdf";s:4:"07f8";s:14:"doc/manual.sxw";s:4:"f7e5";s:35:"sv1/class.tx_svconnectorsql_sv1.php";s:4:"bd74";}',
-  'suggests' => 
+  'suggests' =>
   array (
   ),
   'comment' => 'Verified compatibility with TYPO3 CMS 6.1',
